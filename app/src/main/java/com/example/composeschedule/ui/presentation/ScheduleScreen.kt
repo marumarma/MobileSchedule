d0334cd91d35ff10b270118facad49b74f42949f
khokh <mariaborisovna24@mail.ru>
2023-03-13 23:37:58 +0700

    Реализовано распределение пар по неделе

--- app/src/main/java/com/example/composeschedule/ui/presentation/ScheduleScreen.kt
+++ app/src/main/java/com/example/composeschedule/ui/presentation/ScheduleScreen.kt
@@ -1,5 +1,6 @@
 package com.example.composeschedule.ui.presentation
 
+import android.icu.text.SimpleDateFormat
 import android.os.Bundle
 import android.util.Log
 import androidx.compose.foundation.*
@@ -73,11 +74,15 @@ fun ScheduleScreen(navController: NavController, id : Bundle) {
                         })
                 )
             }
+            //туть
+            var simpleDateFormat = SimpleDateFormat("yyyy-MM-dd")
+            var dataString = simpleDateFormat.format(day)
 
             SingleRowCalendar(
                 onSelectedDayChange = {
                     day = it
                     viewModel.getClass(data = it)
+                    dataString = simpleDateFormat.format(day)
                 }, selectedDayBackgroundColor = Color(0, 108, 190),
             )
             Box(
@@ -91,13 +96,22 @@ fun ScheduleScreen(navController: NavController, id : Bundle) {
                         .fillMaxWidth()
                         .verticalScroll(
                             rememberScrollState()
-                        )
+                        ),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
+                    var weekDay = day.toString().substring(0, day.toString().length-31)
+                    var month = day.toString().substring(4, day.toString().length-27)
+                    var num = day.toString().substring(8, day.toString().length-24)
+                    var year = day.toString().substring(30, day.toString().length)
                     val state = viewModel.clas.collectAsState().value
                     if(state.size > 0){
                         for(i in 0..state.size-1){
-                            getCard(viewModel(), i)
+                            getCard(viewModel(), i, day)
                         }
+                    }else{
+                        emptySchedule()
+
                     }
 
                 }
@@ -142,7 +156,11 @@ fun headcl(viewModel: ScheduleViewModel){
 }
 
 @Composable
-fun getCard(viewModel: ScheduleViewModel, i: Int){
+fun getCard(viewModel: ScheduleViewModel, i: Int, day : Date){
+    var weekDay = day.toString().substring(0, day.toString().length-31)
+    var month = day.toString().substring(4, day.toString().length-27)
+    var num = day.toString().substring(8, day.toString().length-24)
+    var year = day.toString().substring(30, day.toString().length)
 
     val state = viewModel.clas.collectAsState().value
     var subjectName = if(state.size > 0) state[i].subject.name else " "
@@ -162,28 +180,217 @@ fun getCard(viewModel: ScheduleViewModel, i: Int){
         }
     }else null
 
-    if (number == 1 || number == 8 || number == 15 || number == 22 || number == 29 || number == 36){
-        lesNumber = "1-я пара"
-    }
-    else if(number == 2 || number == 9 || number == 16 || number == 23 || number == 30 || number == 37) {
-        lesNumber = "2-я пара"
-    }
-    else if (number == 3 || number == 10 || number == 17 || number == 24 || number == 31 || number == 38){
-        lesNumber = "3-я пара"
-    }
-    else if (number == 4 || number == 11 || number == 18 || number == 25 || number == 32 || number == 39){
-        lesNumber = "4-я пара"
-    }
-    else if (number == 5 || number == 12 || number == 19 || number == 26 || number == 33 || number == 40){
-        lesNumber = "5-я пара"
-    }
-    else if (number == 6 || number == 13 || number == 20 || number == 27 || number == 34 || number == 41){
-        lesNumber = "6-я пара"
-    }
-    else if (number == 7 || number == 14 || number == 21 || number == 28 || number == 35 || number == 42){
-        lesNumber = "7-я пара"
+    if (weekDay == "Mon" && (number == 1 || number == 2 || number == 3 || number == 4 || number == 5 || number == 6 || number == 7) ){
+        if (number == 1){
+            lesNumber = "1-я пара"
+        }
+        else if(number == 2) {
+            lesNumber = "2-я пара"
+        }
+        else if (number == 3){
+            lesNumber = "3-я пара"
+        }
+        else if (number == 4 ){
+            lesNumber = "4-я пара"
+        }
+        else if (number == 5 ){
+            lesNumber = "5-я пара"
+        }
+        else if (number == 6 ){
+            lesNumber = "6-я пара"
+        }
+        else if (number == 7){
+            lesNumber = "7-я пара"
+        }
+
+        oneCard(
+            subjectName = subjectName,
+            teacherName = teacherName,
+            audName = audName,
+            buildName = buildName,
+            groupName = groupName,
+            startTime = startTime,
+            endTime = endTime,
+            lesNumber = lesNumber
+        )
+
+
+    } else if(weekDay == "Tue" && (number == 8 || number == 9 || number == 10 || number == 11 || number == 12 || number == 13 || number == 14)){
+        if (number == 8){
+            lesNumber = "1-я пара"
+        }
+        else if(number == 9) {
+            lesNumber = "2-я пара"
+        }
+        else if (number == 10){
+            lesNumber = "3-я пара"
+        }
+        else if (number == 11 ){
+            lesNumber = "4-я пара"
+        }
+        else if (number == 12 ){
+            lesNumber = "5-я пара"
+        }
+        else if (number == 13 ){
+            lesNumber = "6-я пара"
+        }
+        else if (number == 14){
+            lesNumber = "7-я пара"
+        }
+        oneCard(
+            subjectName = subjectName,
+            teacherName = teacherName,
+            audName = audName,
+            buildName = buildName,
+            groupName = groupName,
+            startTime = startTime,
+            endTime = endTime,
+            lesNumber = lesNumber
+        )
+    }else if(weekDay == "Wed" && (number == 15 || number == 16 || number == 17 || number == 18 || number == 19 || number == 20 || number == 21)){
+        if (number == 15){
+            lesNumber = "1-я пара"
+        }
+        else if(number == 16) {
+            lesNumber = "2-я пара"
+        }
+        else if (number == 17){
+            lesNumber = "3-я пара"
+        }
+        else if (number == 18 ){
+            lesNumber = "4-я пара"
+        }
+        else if (number == 19 ){
+            lesNumber = "5-я пара"
+        }
+        else if (number == 20 ){
+            lesNumber = "6-я пара"
+        }
+        else if (number == 21){
+            lesNumber = "7-я пара"
+        }
+        oneCard(
+            subjectName = subjectName,
+            teacherName = teacherName,
+            audName = audName,
+            buildName = buildName,
+            groupName = groupName,
+            startTime = startTime,
+            endTime = endTime,
+            lesNumber = lesNumber
+        )
+    }else if(weekDay == "Thu" && (number == 22 || number == 23 || number == 24 || number == 25 || number == 26 || number == 27 || number == 28)){
+        if (number == 22){
+            lesNumber = "1-я пара"
+        }
+        else if(number == 23) {
+            lesNumber = "2-я пара"
+        }
+        else if (number == 24){
+            lesNumber = "3-я пара"
+        }
+        else if (number == 25 ){
+            lesNumber = "4-я пара"
+        }
+        else if (number == 26 ){
+            lesNumber = "5-я пара"
+        }
+        else if (number == 27 ){
+            lesNumber = "6-я пара"
+        }
+        else if (number == 28){
+            lesNumber = "7-я пара"
+        }
+        oneCard(
+            subjectName = subjectName,
+            teacherName = teacherName,
+            audName = audName,
+            buildName = buildName,
+            groupName = groupName,
+            startTime = startTime,
+            endTime = endTime,
+            lesNumber = lesNumber
+        )
+    }else if(weekDay == "Fri" && (number == 29 || number == 30 || number == 31 || number == 32 || number == 33 || number == 34 || number == 35)){
+        if (number == 29){
+            lesNumber = "1-я пара"
+        }
+        else if(number == 30) {
+            lesNumber = "2-я пара"
+        }
+        else if (number == 31){
+            lesNumber = "3-я пара"
+        }
+        else if (number == 32 ){
+            lesNumber = "4-я пара"
+        }
+        else if (number == 33 ){
+            lesNumber = "5-я пара"
+        }
+        else if (number == 34 ){
+            lesNumber = "6-я пара"
+        }
+        else if (number == 35){
+            lesNumber = "7-я пара"
+        }
+        oneCard(
+            subjectName = subjectName,
+            teacherName = teacherName,
+            audName = audName,
+            buildName = buildName,
+            groupName = groupName,
+            startTime = startTime,
+            endTime = endTime,
+            lesNumber = lesNumber
+        )
+    }else if(weekDay == "Sat" && (number == 36 || number == 37 || number == 38 || number == 39 || number == 40 || number == 41 || number == 42)){
+        if (number == 36){
+            lesNumber = "1-я пара"
+        }
+        else if(number == 37) {
+            lesNumber = "2-я пара"
+        }
+        else if (number == 38){
+            lesNumber = "3-я пара"
+        }
+        else if (number == 39 ){
+            lesNumber = "4-я пара"
+        }
+        else if (number == 40 ){
+            lesNumber = "5-я пара"
+        }
+        else if (number == 41 ){
+            lesNumber = "6-я пара"
+        }
+        else if (number == 41){
+            lesNumber = "7-я пара"
+        }
+        oneCard(
+            subjectName = subjectName,
+            teacherName = teacherName,
+            audName = audName,
+            buildName = buildName,
+            groupName = groupName,
+            startTime = startTime,
+            endTime = endTime,
+            lesNumber = lesNumber
+        )
+    }else if(weekDay == "Sun"){
+
+        emptySchedule()
     }
 
+
+
+}
+
+@Composable
+fun emptySchedule(){
+    Text(text = "Можно поспать (ﾉ｡• ᵕ •｡)ﾉ*:･ﾟ✧", color = Color(0, 108, 190), fontSize = 20.sp, modifier = Modifier.padding(top = 140.dp))
+}
+
+@Composable
+fun oneCard(subjectName: String, teacherName: String, audName: String, buildName: String, groupName: String, startTime: String, endTime: String, lesNumber: String){
     Card(
         modifier = Modifier
             .fillMaxWidth()
